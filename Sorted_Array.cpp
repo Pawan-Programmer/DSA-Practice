1a8b2c951240b1ef83fa023e2241e5406b6a39a4
Pawan Kumar <krpawan0308@gmail.com>
2026-07-31 09:06:09 +0530

    Solve sorting Problem

--- Sorted_Array.cpp
+++ Sorted_Array.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
-int sorted(){
+int main(){
     int n = 5,flag=0;
-    int Arr[5] = {2,3,4,5,6};
+    int Arr[5] = {10,9,8,7,6};
     for(int i=0;i<n;i++){
         if(Arr[i]<Arr[i+1]){
             flag = 1;
@@ -19,11 +19,5 @@ int sorted(){
     else{
         cout << "Array is sorted in decreasing order" << endl;
     }
-    return sorted();
-
-}
-int main(){
-    sorted();
     return 0;
-
 }
