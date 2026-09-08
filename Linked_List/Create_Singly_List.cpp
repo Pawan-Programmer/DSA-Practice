cbcf3b05dd741f4c90596fb66a9b1c3793816cb5
Pawan Kumar <krpawan0308@gmail.com>
2026-09-08 10:32:20 +0530

    Adding linked list file

---
+++ Linked_List/Create_Singly_List.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* head = new Node{10, nullptr};
+    head->next = new Node{20, nullptr};
+    head->next->next = new Node{30, nullptr};
+    head->next->next->next = new Node{40, nullptr};
+    head->next->next->next->next = new Node{50, nullptr};
+
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
