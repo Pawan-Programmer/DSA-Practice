f5d3388ef7e0a0a59e97aca848189bbf2fe01f8b
Pawan Kumar <krpawan0308@gmail.com>
2026-09-17 22:55:37 +0530

    Problem Solved Successfully

---
+++ LeetCode Problems/1342_No_Of_Steps.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int numberOfSteps(int num){
+    int count = 0;
+    while (num > 0){
+        if (num % 2 == 0){
+            num = num / 2;
+            count++;
+        }
+        else if (num % 2 != 0){
+            num -= 1;
+            count++;
+        }
+    }
+    cout << count ;
+    return 0;
+}
+
+int main(){
+    int num;
+    cout << "Enter the number: " << endl;
+    cin >> num;
+    numberOfSteps(num);
+    return 0;
+}
