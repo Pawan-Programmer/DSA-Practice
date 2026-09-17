1a9da37b5533a8cbae3f3fc15f7e6f0209786473
Pawan Kumar <krpawan0308@gmail.com>
2026-09-17 19:37:51 +0530

    2235 Problem Successfully Done

---
+++ LeetCode Problems/2235_AddTwoNumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int sum(int num1 , int num2){
+    int total = num1 + num2;
+    cout << total;
+    return 0;
+}
+
+int main(){
+    int num1 , num2;
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2 ;
+    sum(num1 , num2);
+}
+
+
+
